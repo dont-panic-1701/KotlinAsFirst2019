406cec0289ed6482522c81a22ac2a99d5014bba8
Marina <birisheva.mm@edu.spbstu.ru>
2019-09-28 01:32:36 +0300

    что-то сделано в 5 уроке

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -165,7 +165,16 @@ fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = a.toSet().int
  *     mapOf("Emergency" to "911", "Police" to "02")
  *   ) -> mapOf("Emergency" to "112, 911", "Police" to "02")
  */
-fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> = TODO()
+fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> {
+    val answer = mapA.toMutableMap()
+    for ((key, value) in mapB) {
+        if (key in mapA && mapA[key] != value)
+            answer[key] += ", $value"
+        if (key !in mapA)
+            answer[key] = value
+    }
+    return answer
+}
 
 /**
  * Средняя
